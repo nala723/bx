f03f2b42aaf4509cd1575d88b3ef2fb5fb551809
kindtiger <seonbeom2@gmail.com>
2025-02-22 15:29:24 +0900

    1차 컨펌

--- src/intro.js
+++ src/intro.js
@@ -1,18 +1,12 @@
-import '../lib/smooth';
-import './styles/style.css';
+import "../lib/smooth";
+import { markers } from "../lib/smooth";
 
-
-
-// import { gsap } from "gsap";
+import { gsap } from "gsap";
 // import { Draggable } from "gsap/Draggable";
 // import { MotionPathPlugin } from "gsap/MotionPathPlugin";
 // import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 // gsap.registerPlugin(ScrollTrigger,Draggable,MotionPathPlugin);
-
-
-
-
 
 // 1. 터미널 창에서 npm i 실행한다.
 // 2. 깃헙에 올린다.
@@ -22,6 +16,84 @@ import './styles/style.css';
 
 
 
+// gsap.from('.contents *',{
+//   stagger:0.3,
+//   delay:2,
+//   y:30,
+//   opacity:0
+// })
+
+
+const enter = document.querySelector('.enter');
+
+
+
+const intro = document.querySelector('.intro');
+
+
+
+
+enter.addEventListener('click',()=>{
+
+  const tl = gsap.timeline();
+
+
+  tl.to('.small-circle',{
+    scale:1.5,
+    opacity:0
+  })
+
+  tl.to(intro,{
+    opacity:0,
+    onComplete:()=>{
+      
+      intro.remove();
+
+      gsap.from('.background img',{
+        opacity: 0,
+        stagger: 0.3  
+      })
+    }
+  })
+})
+
+
+
+
+
+gsap.from('.background img',{
+  duration:2,
+  opacity: 0,
+  stagger: 0.3  
+})
+
+
+
+
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu');
+
+
+
+let clicked = false;
+
+hamburger.addEventListener('click',()=>{
+  
+  
+  if(!clicked){
+    gsap.to(menu,{
+      height:'100vh'
+    })
+  }else{
+    gsap.to(menu,{
+      height:0
+    })
+  }
+
+  clicked = !clicked
+  
+  
+})
 
 
 
@@ -35,11 +107,4 @@ import './styles/style.css';
 
 
 
-
-
-
-
-
-
-
-
+markers();
